86ff787611490e8ca0d40fc7444cfc03f9279d88
Connor Griffith <connor.griffith@berkeley.edu>
2019-11-07 02:13:46 -0800

    Mostly done with getting kobukis to talk to each other.  Need to see exactly how to listen to one kobuki

--- ble_adv_listen_template(Follower)/main.c
+++ ble_adv_listen_template(Follower)/main.c
@@ -15,16 +15,16 @@ static simple_ble_config_t ble_config = {
         .platform_id       = 0x49,    // used as 4th octet in device BLE address
         .device_id         = 0x0005,  // Last two octets of device address
         .adv_name          = "EE149", // irrelevant in this example
-        .adv_interval      = MSEC_TO_UNITS(1000, UNIT_0_625_MS), // send a packet once per second (minimum is 20 ms)
-        .min_conn_interval = MSEC_TO_UNITS(500, UNIT_1_25_MS), // irrelevant if advertising only
-        .max_conn_interval = MSEC_TO_UNITS(1000, UNIT_1_25_MS), // irrelevant if advertising only
+        .adv_interval      = MSEC_TO_UNITS(100, UNIT_0_625_MS), // send a packet once per second (minimum is 20 ms)
+        .min_conn_interval = MSEC_TO_UNITS(50, UNIT_1_25_MS), // irrelevant if advertising only
+        .max_conn_interval = MSEC_TO_UNITS(80, UNIT_1_25_MS), // irrelevant if advertising only
 };
 simple_ble_app_t* simple_ble_app;
 
 // TODO: implement BLE advertisement callback
 void ble_evt_adv_report(ble_evt_t const* p_ble_evt) {
   ble_gap_addr_t address = p_ble_evt->evt.gap_evt.params.adv_report.peer_addr;
-  if (address.addr[0] == 0xFD && address.addr[5] == 0xC0 && address.addr[3] == 0xE5) {
+  if (address.addr[0] == 0xFF && address.addr[5] == 0xC0 && address.addr[3] == 0xE5) {
     //printf("%x:%x:%x:%x:%x:%x\n", address.addr[0], address.addr[1], address.addr[2], address.addr[3], address.addr[4], address.addr[5]);
     ble_data_t data = p_ble_evt->evt.gap_evt.params.adv_report.data;
     uint8_t* data_addr = data.p_data;
@@ -71,12 +71,45 @@ void ble_evt_adv_report(ble_evt_t const* p_ble_evt) {
 
 int main(void) {
 
+  // initialize display
+  nrf_drv_spi_t spi_instance = NRF_DRV_SPI_INSTANCE(1);
+  nrf_drv_spi_config_t spi_config = {
+    .sck_pin = BUCKLER_LCD_SCLK,
+    .mosi_pin = BUCKLER_LCD_MOSI,
+    .miso_pin = BUCKLER_LCD_MISO,
+    .ss_pin = BUCKLER_LCD_CS,
+    .irq_priority = NRFX_SPI_DEFAULT_CONFIG_IRQ_PRIORITY,
+    .orc = 0,
+    .frequency = NRF_DRV_SPI_FREQ_4M,
+    .mode = NRF_DRV_SPI_MODE_2,
+    .bit_order = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST
+  };
+  error_code = nrf_drv_spi_init(&spi_instance, &spi_config, NULL, NULL);
+  APP_ERROR_CHECK(error_code);
+  display_init(&spi_instance);
+  printf("Display initialized!\n");
+
+    // initialize i2c master (two wire interface)
+  nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
+  i2c_config.scl = BUCKLER_SENSORS_SCL;
+  i2c_config.sda = BUCKLER_SENSORS_SDA;
+  i2c_config.frequency = NRF_TWIM_FREQ_100K;
+  error_code = nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
+  APP_ERROR_CHECK(error_code);
+  mpu9250_init(&twi_mngr_instance);
+  printf("IMU initialized!\n");
+
+  // initialize Kobuki
+  kobukiInit();
+  printf("Kobuki initialized!\n");
+
   // Setup BLE
   // Note: simple BLE is our own library. You can find it in `nrf5x-base/lib/simple_ble/`
   simple_ble_app = simple_ble_init(&ble_config);
   advertising_stop();
 
   // TODO: Start scanning
+  //BLE Event above will now be called whenever there is an event to decode
   scanning_start();
 
   while(1) {
