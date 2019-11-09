a0c9be175476f0465d6bbf5ae9cec8d104d61a6f
AmiraRyce <amira.ryce@berkeley.edu>
2019-11-08 20:00:42 -0800

    add all

---
+++ ble_adv_template/states.h
@@ -0,0 +1,21 @@
+/*
+ * states.h
+ *
+ *  Created on: Sep 22, 2018
+ *      Author: shromonaghosh
+ */
+
+#ifndef STATES_H_
+#define STATES_H_
+
+#include <stdio.h>
+
+typedef enum {
+    OFF=0,
+    FORWARD,
+    BACKWARD,
+    RIGHT,
+    LEFT
+} states;
+
+#endif /* STATES_H_ */
